e11cedb54bdc5f7d4e6d59dcb33c6303c01fc9de
Anderson Souza <andersonsouza007@live.com>
2022-07-19 20:33:05 -0300

    Twig

--- core/Twig.php
+++ core/Twig.php
@@ -2,19 +2,22 @@
 
 namespace core;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class Twig{
 
     private $twig;
     private $functions = [];
   
     public function loadTwig(){
-        $this->twig = new \Twig_Environment($this->loadViews(), [
+        $this->twig = new Environment($this->loadViews(), [
         'debug' => true,
-        'auto_reload' => true,
+        'auto_reload' => true
         ]);
         return $this->twig;
    }
    private function loadViews(){
-    return new \Twig_Loader_Filesystem('../app/views');
+    return new FilesystemLoader('../app/views');
    }
 }
